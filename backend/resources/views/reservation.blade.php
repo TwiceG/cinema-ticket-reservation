d4734696fe79407e7511724a6ffe18c0d5858431
TwiceG <g0stron0g@gmail.com>
2023-07-08 13:14:49 +0200

    install laravel project

---
+++ backend/resources/views/reservation.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Reserve</title>
+<head/>
+<body>
+<h1>Reserve your seats</h1>
+<p1>Available seats:</p1>
+<form action="/reserve">
+
+</form>
+
+</body>
+</html>
